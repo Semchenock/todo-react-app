fc2065c3e8f01ac49674ecbf087cd5d9c13e1eb0
Semchenock <64313471+Semchenock@users.noreply.github.com>
2022-07-28 14:35:56 +0300

    added unit test for form

--- src/components/TodoForm.test.tsx
+++ src/components/TodoForm.test.tsx
@@ -1,8 +1,35 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import TodoForm from "./TodoForm";
-
-test("renders component", () => {
-  render(<TodoForm />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import TodoList from "./TodosList";
+describe("TodoForm component", () => {
+  test("button is disebled with no input", () => {
+    render(<TodoForm />);
+    const button = screen.getByRole("button");
+    expect(button).toHaveAttribute("disabled");
+  });
+  test("button is disabled with wrong input", () => {
+    render(<TodoForm />);
+    const input = screen.getByPlaceholderText("What needs to be done?");
+    const button = screen.getByRole("button");
+    userEvent.type(input, "  ");
+    expect(button).toHaveAttribute("disabled");
+  });
+  test("button is clickable with right input", () => {
+    render(<TodoForm />);
+    const input = screen.getByPlaceholderText("What needs to be done?");
+    const button = screen.getByRole("button");
+    userEvent.type(input, "Test");
+    expect(button).not.toHaveAttribute("disabled");
+  });
+  //   test("task added when form submited", () => {
+  //     render(<TodoForm />);
+  //     render(<TodoList />);
+  //     const input = screen.getByPlaceholderText("What needs to be done?");
+  //     const button = screen.getByRole("button");
+  //     userEvent.type(input, "Test");
+  //     userEvent.click(button);
+  //     const task = screen.getByText("Test");
+  //     expect(task).toBeInTheDocument();
+  //   });
 });
